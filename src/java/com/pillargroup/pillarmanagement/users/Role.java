b52d4d79b7779fa67db517a7639e547f0bf07e39
NeryCoy <jguerra-2026492@kinal.edu.gt>
2026-09-17 16:32:43 -0600

    Role implementado

--- src/java/com/pillargroup/pillarmanagement/users/Role.java
+++ src/java/com/pillargroup/pillarmanagement/users/Role.java
@@ -9,6 +9,27 @@ package java.com.pillargroup.pillarmanagement.users;
  * @author informatica
  */
 public class Role {
-    roleId
-    
+    String roleId;
+    String roleName;
+
+    public Role(String roleId, String roleName) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
