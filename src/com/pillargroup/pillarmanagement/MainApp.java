cdf8266d7f37cd407f5038ae9201d4722c8fa6e1
aaronsebb <aaronsebmol@icloud.com>
2026-09-16 22:07:44 -0600

    implementacion del metodo start de javafx

--- src/com/pillargroup/pillarmanagement/MainApp.java
+++ src/com/pillargroup/pillarmanagement/MainApp.java
@@ -1,20 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package com.pillargroup.pillarmanagement;
 
-/**
- *
- * @author aaron
- */
-public class MainApp {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-    /**
-     * @param args the command line arguments
-     */
+public class MainApp extends Application {
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        launch(args);
+        
     }
     
+    @Override
+    public void start(Stage stage){
+   
+        
+    }
 }
