07384afdccb2002e5c99c21f94d1d75d48a5b13d
unknown <kevinajurod@gmail.com>
2026-09-17 16:32:36 -0600

    implementación conexión a BD

--- src/java/com/pillargroup/pillarmanagement/MainApp.java
+++ src/java/com/pillargroup/pillarmanagement/MainApp.java
@@ -8,14 +8,11 @@ public class MainApp extends Application {
     public static void main(String[] args) {
 
         launch(args);
-        System.out.println("Hola mundo");
         
     }
     
     @Override
     public void start(Stage stage){
-   
-        System.out.println("Hola weco");
         
     }
 }
