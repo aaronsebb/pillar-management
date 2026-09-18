fa4af26be1a2500851635e614a641911850c390f
Informatica <cisaacap@gmail.com>
2026-09-18 14:28:36 -0600

    implementacion de excepciones

---
+++ src/java/com/pillargroup/pillarmanagement/exception/ServiceException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java.com.pillargroup.pillarmanagement.exception;
+
+/**
+ *
+ * @author informatica
+ */
+public class ServiceException extends Exception {
+    public ServiceException(String message){
+        super (message);
+    }
+}
